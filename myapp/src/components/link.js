c22b8b7cd4dbe4016f4cbd26f7fabd11da1b268a
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2025-01-30 16:41:14 +0530

    Update link.js

--- myapp/src/components/link.js
+++ myapp/src/components/link.js
@@ -1,8 +1,10 @@
-const links = [
-    { to: '/', text: 'Home' },
-    { to: '/experience', text: 'Experience' },
-    { to: '/projects', text: 'Projects' },
-    { to: '/contact', text: 'Contact' },
-  ];
+import { FaHome, FaUserAlt, FaCog, FaBriefcase } from 'react-icons/fa';
 
-  export default links;
+const links = [
+  { to: '/', text: 'Home', icon: <FaHome /> },
+  { to: '/profile', text: 'Profile', icon: <FaUserAlt /> },
+  { to: '/settings', text: 'Settings', icon: <FaCog /> },
+  { to: '/portfolio', text: 'Portfolio', icon: <FaBriefcase /> },
+];
+
+export default links;
