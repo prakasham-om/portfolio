69f1b18f99038403de3d42543b769974751cea0b
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2025-02-13 11:52:43 +0530

    Update Header.jsx

--- myapp/src/components/Header.jsx
+++ myapp/src/components/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
   return (
     <header className="bg-gray-900 text-white py-4 sticky top-0 z-50 shadow-lg">
       <div className="px-4 mx-auto flex justify-between items-center max-w-6xl">
-        <h1 className="text-xl font-signature font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent">
-         <GiCarnivalMask />
-        </h1>
+       < GiCarnivalMask  className="text-xl font-signature font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent" />
+          
+        
 
         {/* Navigation Links */}
         <nav className="hidden md:flex space-x-6">
